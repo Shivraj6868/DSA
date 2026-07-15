626baa2557f076b9e61b75dd436ca6167adeb47f
Shivraj Pawar <shivrajpawar6868@gmail.com>
2026-07-15 23:30:02 +0530

    Solve Codechef Question on Finding maximum Number in Array

---
+++ CodeChef/Find maximum in an Array.py
@@ -0,0 +1,36 @@
+# Find maximum Number in an Array.
+
+# Solution cointain Time Complexity O(n^2) 
+n = int(input())
+arr = list(map(int, input().split()))
+
+max_num = arr[0]
+
+for i in range(n):
+    for j in range(n):
+
+        if arr[i] > arr[j]:
+            max_num = arr[i]
+            j+1
+
+        if arr[i] < arr[j]:
+            max_num = arr[j]
+            i = j
+            j+1
+
+print(max_num)
+
+
+# optimized solution, Time complexity O(n)
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+max_num = arr[0]
+
+for i in range(n):
+    if arr[i] > max_num:
+        max_num = arr[i]
+
+print(max_num)
+
