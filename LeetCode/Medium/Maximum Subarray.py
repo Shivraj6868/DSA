be34c1e0784074f2b09df8ec29a0f915fcaa5cc8
Shivraj Pawar <shivrajpawar6868@gmail.com>
2026-07-20 23:34:29 +0530

    53.Maximum Subarray

---
+++ LeetCode/Medium/Maximum Subarray.py
@@ -0,0 +1,15 @@
+# 53. Maximum Subarray
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_sum = nums[0]
+        current_sum = 0
+
+        for i in nums:
+            current_sum += i
+            max_sum = max(max_sum, current_sum)
+
+            if current_sum < 0:
+                current_sum = 0
+
+        return max_sum
